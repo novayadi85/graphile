bf7588da798117d413fd9379485d58c928b97733
Komang Novayadi <novayadi85@gmail.com>
2020-08-07 12:58:12 +0800

    Initialize postgraphile-46

---
+++ @app/server/src/plugins/PrimaryKeyMutationsOnlyPlugin.ts
@@ -0,0 +1,26 @@
+import { Plugin } from "graphile-build";
+
+type PgConstraint = any;
+
+const PrimaryKeyMutationsOnlyPlugin: Plugin = builder => {
+  builder.hook(
+    "build",
+    build => {
+      if (!build.pgIntrospectionResultsByKind)
+        throw new Error("Plugin loaded too early");
+      build.pgIntrospectionResultsByKind.constraint.forEach(
+        (constraint: PgConstraint) => {
+          if (!constraint.tags.omit && constraint.type !== "p") {
+            constraint.tags.omit = ["update", "delete"];
+          }
+        }
+      );
+      return build;
+    },
+    [],
+    [],
+    ["PgIntrospection"]
+  );
+};
+
+export default PrimaryKeyMutationsOnlyPlugin;
